6062efeb2ca4d9859a52125c9e6f98c8ef970a62
Agustina Sanchez <tinasanchez2105@gmail.com>
2025-04-01 18:43:29 -0300

    Corregi indentacion

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
 
-app.use(express.json()); 
-app.use('/api/products', productsRouter); 
-app.use('/api/carts', cartsRouter);       
+app.use(express.json());
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.use((req, res) => {
     res.status(404).send('Ruta no encontrada');
